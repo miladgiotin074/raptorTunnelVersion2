dfd38c22fe9fd816fb48946f8ecefacf6847e07f
miladgiotin074 <miladgiotin074@gmail.com>
2025-08-26 13:37:23 +0330

    local fonts

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col md:flex-row`}
+        className="antialiased flex flex-col md:flex-row"
       >
         <Sidebar />
         <main className="flex-1 md:ml-64 p-4 pb-20 md:pb-4">
